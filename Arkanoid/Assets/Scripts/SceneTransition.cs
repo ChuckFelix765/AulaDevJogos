eae0b1159b2ca7825d37d097f8e4da2fe3c4cad2
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-02-26 07:43:45 -0300

    Arrumei
    Consegui arrumar o desktop do pc

---
+++ Arkanoid/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    // Return the current Active Scene in order to get the current Scene name.
+    //Scene scene = SceneManager.GetActiveScene();
+    // Check if the name of the current Active Scene is your first Scene.
+    // if (scene.name == "cena_1"){
+    //     SceneManager.LoadScene("cena_2");
+    // }
+    // else if(scene.name == "cena_2"){
+    //     SceneManager.LoadScene("cena_1");
+    // }
+}
