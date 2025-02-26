e8bf6b030a79ef98b6197027486d17ff629f50ab
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-02-26 10:21:03 -0300

    Cena inicial e final
    Adicionado o Menu, a cena final, display organizado, e mais alguns feitos

--- Arkanoid/Assets/Scripts/GameManager.cs
+++ Arkanoid/Assets/Scripts/GameManager.cs
@@ -24,17 +24,21 @@ public class GameManager : MonoBehaviour
     // Gerência da pontuação e fluxo do jogo
     void OnGUI () {
         GUI.skin = layout;
-        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 350, 100, 100), "" + PlayerScore2);
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
-        {
+        if(PlayerScore2 == 3){
+            
             PlayerScore2 = 0;
+
             theBall.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+
+            GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART");
         }
-        if (PlayerScore2 == 10)
-        {
-            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
-            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-        }
+
+        // if (PlayerScore2 == 10)
+        // {
+        //     GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+        //     theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        // }
     }
 }
