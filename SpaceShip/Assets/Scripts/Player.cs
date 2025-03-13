445138b9250dbaac8eca519572e6ff1f911a0c3b
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-03-13 16:47:56 -0300

    Player
    Player se mexe

--- SpaceShip/Assets/Scripts/Player.cs
+++ SpaceShip/Assets/Scripts/Player.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour{
+    public float moveSpeed = 5f; // Velocidade de movimentação
+    private Rigidbody2D rb; // Referência para o Rigidbody2D
+    private Vector2 moveDirection; // Direção do movimento
+
+    // Start é chamado antes do primeiro frame de atualização
+    void Start(){
+        rb = GetComponent<Rigidbody2D>(); // Pegando o componente Rigidbody2D
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Update é chamado uma vez por quadro
+    void Update(){
+        // Obtendo a entrada do usuário para movimentação
+        float moveX = Input.GetAxis("Horizontal"); // Movimento na horizontal (A/D ou setas)
+        float moveY = Input.GetAxis("Vertical"); // Movimento na vertical (W/S ou setas)
+
+        // Calculando a direção do movimento
+        moveDirection = new Vector2(moveX, moveY).normalized; // Normalizando para evitar velocidade maior na diagonal
+    }
+
+    // FixedUpdate é chamado de forma mais consistente para física
+    void FixedUpdate(){
+        // Aplicando a movimentação no Rigidbody2D
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
 }
