fcf4f91329dcc830be691c898e0d5c2be52a61fa
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-03-20 17:03:21 -0300

    Plataforma
    Inicio do jogo de plataforma

---
+++ Plataforma/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float Horizontali = Input.GetAxis("Horizontal");
+        float Verticali = Input.GetAxis("Vertical");
+        Vector3 direc = new Vector3(Horizontali, Verticali, 0);
+        transform.Translate(direc * 5 * Time.deltaTime);
+    }
+}
