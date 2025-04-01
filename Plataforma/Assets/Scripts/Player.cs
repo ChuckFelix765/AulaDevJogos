cf5d001a8604d8a0e5d12efd8518b615edef3295
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-04-01 10:12:36 -0300

    Updates 2
    Pulo, mapa, e apaguei a cena 1 que apaguei

--- Plataforma/Assets/Scripts/Player.cs
+++ Plataforma/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
     // Start is called before the first frame update
-    //public float jumpForce = 0;
+    public float jumpForce = 0;
 
     private Rigidbody2D rb; // Referência ao Rigidbody2D do jogador
     private Animator animations;
@@ -17,13 +17,14 @@ public class Player : MonoBehaviour{
     void Update(){
         //isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         float Horizontali = Input.GetAxis("Horizontal");
-        float Verticali = Input.GetAxis("Vertical");
-        Vector3 direc = new Vector3(Horizontali, Verticali, 0);
+        Vector3 direc = new Vector3(Horizontali, 0, 0);
         transform.Translate(direc * 5 * Time.deltaTime);
 
-        // if (Input.GetButtonDown("KeyCode.Space") && isGrounded) // Verifica se a tecla de pulo foi pressionada e se está no chão
-        // {
-        //     rb.velocity = Vector2.up * jumpForce;
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){ // Verifica se a tecla de pulo foi pressionada e se está no chão
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     Atira()
         // }
     }
 }
