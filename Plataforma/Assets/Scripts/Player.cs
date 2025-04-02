e09b7d00833a1a77b4b98a84b3f1a7a61f605425
Alan Daiki Suga <110938759+daikisuga@users.noreply.github.com>
2025-04-01 21:11:52 -0300

    animação player

--- Plataforma/Assets/Scripts/Player.cs
+++ Plataforma/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
@@ -8,23 +9,53 @@ public class Player : MonoBehaviour{
 
     private Rigidbody2D rb; // Referência ao Rigidbody2D do jogador
     private Animator animations;
+    private bool grounded;
 
     void Start(){
         rb = GetComponent<Rigidbody2D>();
+        animations = GetComponent<Animator>();
     }
 
     // Update is called once per frame
+
     void Update(){
         //isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         float Horizontali = Input.GetAxis("Horizontal");
         Vector3 direc = new Vector3(Horizontali, 0, 0);
         transform.Translate(direc * 5 * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){ // Verifica se a tecla de pulo foi pressionada e se está no chão
-            rb.velocity = Vector2.up * jumpForce;
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && grounded){ // Verifica se a tecla de pulo foi pressionada e se está no chão
+            jump();
         }
-        // if(Input.GetKeyDown(KeyCode.Space)){
-        //     Atira()
-        // }
+        /*Virar para o lado que está andando*/
+        if(Horizontali>0.01f){
+            transform.localScale = new Vector3(-5, 5, 1);
+        }else if(Horizontali<-0.01f){
+            transform.localScale = new Vector3(5, 5, 1);
+        }else{
+            transform.localScale = transform.localScale;
+        }
+        animations.SetBool("Run", Horizontali !=0);
+        animations.SetBool("grounded", grounded);
+        print(Horizontali!=0);
+
+         if(Input.GetKeyDown(KeyCode.Space)){
+             Atira();
+         }
+    }
+    private void jump(){
+            rb.velocity = Vector2.up * jumpForce;
+            grounded = false;
+        
+    }
+    void Atira(){
+        animations.SetTrigger("atira");
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground"){
+            grounded = true;
+        }
     }
 }
--- Plataforma/Assets/Scripts/alien_mob.cs
+++ Plataforma/Assets/Scripts/alien_mob.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class alien_mob : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    private float velx = 1.0f;
+    private int hp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var vel = rb2d.velocity;
+        vel.x = -velx;
+        rb2d.velocity = vel;
+        transform.position = new Vector3(velx, 0, 0)*Time.deltaTime;
+    
     }
 }
